4c564779dd8128f1594c1aa93283c2aef21be0ad
SooKyoung <sksohn01@naver.com>
2022-07-24 19:26:12 +0900

    2022-07-24-pc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
+// COMPONENTS
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton
+              text={"왼쪽 버튼"}
+              onClick={() => alert("왼쪽 클릭")}
+            ></MyButton>
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("오른쪽 클릭")}
+            ></MyButton>
+          }
+        ></MyHeader>
         <h2>App.js</h2>
+
+        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} />
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        ></MyButton>
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}
+        ></MyButton>
+        <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")}></MyButton>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/new" element={<New></New>}></Route>
+          <Route path="/edit" element={<Edit></Edit>}></Route>
+          <Route path="/diary/:id" element={<Diary></Diary>}></Route>
+        </Routes>
       </div>
     </BrowserRouter>
   );
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,33 @@
+import { useSearchParams, useNavigate } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id: ", id);
+  const mode = searchParams.get("mode");
+  console.log("mode: ", mode);
+
   return (
     <div>
       <h2>Edit</h2>
       <p>이곳은 일기 수정 페이지입니다. </p>
+      <button onClick={() => setSearchParams({ who: "ssk" })}>QS 바꾸기</button>
+      <button
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로 가기
+      </button>
     </div>
   );
 };
