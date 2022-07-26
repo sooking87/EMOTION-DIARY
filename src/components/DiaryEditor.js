b6ae16d1725cb7591b49d6d36ccd9a9dfc6ec271
SooKyoung <sksohn01@naver.com>
2022-07-27 02:24:55 +0900

    2022-07-26-pc

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -5,40 +5,10 @@ import MyButton from "./MyButton";
 import EmotionItem from "./EmotionItem";
 import { DiaryDispatchContext } from "./../App.js";
 import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotionList";
 
-const emotionList = [
-    {
-        emotion_id: 1,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-        emotion_descript: "완전 좋음",
-    },
-    {
-        emotion_id: 2,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-        emotion_descript: "좋음",
-    },
-    {
-        emotion_id: 3,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-        emotion_descript: "그럭저럭",
-    },
-    {
-        emotion_id: 4,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-        emotion_descript: "나름",
-    },
-    {
-        emotion_id: 5,
-        emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-        emotion_descript: "끔찍함",
-    },
-
-]
-
-
-const DiaryEditor = ({isEdit, originData}) => {
-
-  const {onCreate, onEdit} = useContext(DiaryDispatchContext);
+const DiaryEditor = ({ isEdit, originData }) => {
+  const { onCreate, onEdit } = useContext(DiaryDispatchContext);
   const [content, setContent] = useState("");
   const contentRef = useRef();
   const [emotion, setEmotion] = useState(3);
@@ -47,70 +17,99 @@ const DiaryEditor = ({isEdit, originData}) => {
 
   const handleSubmit = () => {
     if (content.length < 1) {
-        contentRef.current.focus();
-        return;
+      contentRef.current.focus();
+      return;
     }
 
-    if (window.confirm(isEdit ? "일기를 수정하시겠습니까? " : "새로운 일기를 작성하시겠습니까? ")) {
-        if (!isEdit) {
-            onCreate(date, content, emotion);
-        }
-        else {
-            onEdit(originData.id, date, content, emotion);
-        }
+    if (
+      window.confirm(
+        isEdit
+          ? "일기를 수정하시겠습니까? "
+          : "새로운 일기를 작성하시겠습니까? "
+      )
+    ) {
+      if (!isEdit) {
+        onCreate(date, content, emotion);
+      } else {
+        onEdit(originData.id, date, content, emotion);
+      }
     }
 
-    navigate('/', {replace: true});
-  }
+    navigate("/", { replace: true });
+  };
 
   const handleClickRemote = (emotion) => {
     setEmotion(emotion);
-  }
+  };
 
-  useEffect(()=>{
-    if(isEdit) {
-        setDate(getStringDate(new Date(parseInt(originData.date))));
-        setEmotion(originData.emotion);
-        setContent(originData.content);
+  useEffect(() => {
+    if (isEdit) {
+      setDate(getStringDate(new Date(parseInt(originData.date))));
+      setEmotion(originData.emotion);
+      setContent(originData.content);
     }
-  },[isEdit, originData])
+  }, [isEdit, originData]);
 
   return (
     <div className="DiaryEditor">
-      <MyHeader headText={isEdit ? "일기 수정하기" :"새 일기쓰기"}
-      leftChild={<MyButton text={"< 뒤로 가기"} onClick={() => navigate(-1)}></MyButton>}></MyHeader>
+      <MyHeader
+        headText={isEdit ? "일기 수정하기" : "새 일기쓰기"}
+        leftChild={
+          <MyButton
+            text={"< 뒤로 가기"}
+            onClick={() => navigate(-1)}
+          ></MyButton>
+        }
+      ></MyHeader>
       <div>
         <section>
           <h4>오늘은 언제인가요?</h4>
           <div className="input_box">
-            <input className="input_date" type="date" value={date} onChange={(e) => setDate(e.target.value)} />
-            </div>
+            <input
+              className="input_date"
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
         </section>
         <section>
-            <h4>오늘의 감정</h4>
-            <div className="input_box emotion_list_wrapper">
-                {emotionList.map((it) => (
-                <EmotionItem key={it.emotion_id} {...it}onClick={handleClickRemote} isSelected={it.emotion_id === emotion}></EmotionItem>
-                ))}
-            </div>
+          <h4>오늘의 감정</h4>
+          <div className="input_box emotion_list_wrapper">
+            {emotionList.map((it) => (
+              <EmotionItem
+                key={it.emotion_id}
+                {...it}
+                onClick={handleClickRemote}
+                isSelected={it.emotion_id === emotion}
+              ></EmotionItem>
+            ))}
+          </div>
         </section>
         <section>
-            <h4>오늘의 일기</h4>
-            <div className="input_box text_wrapper">
-                <textarea 
-                placeholder="오늘은 어땠나요?" 
-                ref={contentRef} value={content} onChange={(e) => setContent(e.target.value)}></textarea>
-            </div>
+          <h4>오늘의 일기</h4>
+          <div className="input_box text_wrapper">
+            <textarea
+              placeholder="오늘은 어땠나요?"
+              ref={contentRef}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            ></textarea>
+          </div>
         </section>
         <section>
-            <div className="control_box">
-                <MyButton text={"취소하기"} onClick={() => navigate(-1)}></MyButton>
-                <MyButton text={"작성완료"} type={"positive"} onClick={handleSubmit}></MyButton>
-            </div>
+          <div className="control_box">
+            <MyButton text={"취소하기"} onClick={() => navigate(-1)}></MyButton>
+            <MyButton
+              text={"작성완료"}
+              type={"positive"}
+              onClick={handleSubmit}
+            ></MyButton>
+          </div>
         </section>
       </div>
     </div>
   );
-}
+};
 
 export default DiaryEditor;
--- src/pages/Diary.js
+++ src/pages/Diary.js
@@ -1,44 +1,77 @@
 import { useContext, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { DiaryDispatchContext } from "../App";
+import { DiaryStateContext } from "../App";
 import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotionList";
 import MyHeader from "../components/MyHeader";
 import MyButton from "../components/MyButton";
 
 const Diary = () => {
-
   const { id } = useParams();
-  const diaryList = useContext(DiaryDispatchContext);
+  const diaryList = useContext(DiaryStateContext);
   const navigate = useNavigate();
   const [data, setDate] = useState();
 
   useEffect(() => {
-    if (diaryList >= 1) {
-      const targetDiary = diaryList.find((it) => parseInt(it.id) === parseInt(id));
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
 
       if (targetDiary) {
         setDate(targetDiary);
-      }
-      else {
+      } else {
         alert("없는 일기입니다.");
-        navigate('/', {replace:true});
+        navigate("/", { replace: true });
       }
     }
   }, [id, diaryList]);
 
   if (!data) {
     return <div className="DiaryPage">로딩중입니다,,,</div>;
-  }
-  else {
+  } else {
+    const curEmotionData = emotionList.find(
+      (it) => parseInt(it.emotion_id) === parseInt(data.emotion)
+    );
+
     return (
       <div className="DiaryPage">
-        <MyHeader headText={`${getStringDate(new Date(data.date))} 기록`}
-        leftChild={<MyButton text={"< 뒤로가기"} onClick={()=>navigate(-1)} />}
-        rightChild={<MyButton text={"수정"} onClick={() => navigate(`/edit/${data.id}`)}/>}
-        >
-        </MyHeader>
+        <MyHeader
+          headText={`${getStringDate(new Date(data.date))} 기록`}
+          leftChild={
+            <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+          }
+          rightChild={
+            <MyButton
+              text={"수정하기"}
+              onClick={() => navigate(`/edit/${data.id}`)}
+            />
+          }
+        ></MyHeader>
+        <article>
+          <section>
+            <h4>오늘의 감정</h4>
+            <div
+              className={[
+                "diary_img_wrapper",
+                `diary_img_wrapper_${data.emotion}`,
+              ].join(" ")}
+            >
+              <img src={curEmotionData.emotion_img} />
+              <div className="emotion_descript">
+                {curEmotionData.emotion_descript}
+              </div>
+            </div>
+          </section>
+          <section>
+            <h4>오늘의 일기</h4>
+            <div className="diary_content_wrapper">
+              <p>{data.content}</p>
+            </div>
+          </section>
+        </article>
       </div>
-    )
+    );
   }
 };
 
