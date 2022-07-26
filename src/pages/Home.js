3a41d6cd13cbe256f58aafd1049531ddd50ea85a
SooKyoung <sksohn01@naver.com>
2022-07-26 13:38:38 +0900

    2022-07-26-pc

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,64 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 // COMPONENTS
 import MyHeader from "./../components/MyHeader";
 import MyButton from "./../components/MyButton";
+import DiaryList from "../components/DiaryList";
 import { DiaryStateContext } from "../App";
 
 const Home = () => {
   const diaryList = useContext(DiaryStateContext);
-  
+
+  // 현재 월에 해당하는 일기만 필요하므로 useState 사용
+  const [data, setData] = useState([]);
+
   const [curDate, setCurDate] = useState(new Date());
-  
-  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`
+
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  // 해당 달에 작성된 일기만 추리기 위한 코드
+  useEffect(() => {
+    if (diaryList.length >= 1) {
+      const firstDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth(),
+        1
+      ).getTime();
+      const lastDay = new Date(
+        curDate.getFullYear(),
+        curDate.getMonth() + 1,
+        0
+      ).getTime();
+
+      setData(
+        diaryList.filter((it) => firstDay <= it.date && it.date <= lastDay)
+      );
+    }
+  }, [diaryList, curDate]);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
   // 버튼 클릭 -> 월이 바뀔 수 있도록 하는 메서드
   const increaseMonth = () => {
-    setCurDate(new Date(curDate.getFullYear(), curDate.getMonth() + 1), curDate.getDate())
-  }
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1),
+      curDate.getDate()
+    );
+  };
   const decreaseMonth = () => {
-    setCurDate(new Date(curDate.getFullYear(), curDate.getMonth() - 1), curDate.getDate())
-  }
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1),
+      curDate.getDate()
+    );
+  };
   return (
     <div>
-      <MyHeader 
-      headText={headText}
-      leftChild={<MyButton text={"<"} onClick={decreaseMonth}/>}
-      rightChild={<MyButton text={">"} onClick={increaseMonth}/>}
-      >
-      </MyHeader>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={">"} onClick={increaseMonth} />}
+      ></MyHeader>
+      <DiaryList diaryList={data}></DiaryList>
     </div>
   );
 };
