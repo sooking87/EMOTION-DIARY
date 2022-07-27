f9c3a5c996ca59fb941ef9786a5efb78166f288d
sooking87 <sksohn01@naver.com>
2022-07-27 13:46:50 +0900

    2022-07-27-laptop

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,7 +1,13 @@
 import DiaryEditor from "../components/DiaryEditor";
-import React from "react";
+import React, {useEffect} from "react";
 
 const New = () => {
+  // 페이지 별 타이틀 수정하기
+  useEffect(() => {
+    const titleElem = document.getElementsByTagName('title')[0];
+    titleElem.innerHTML = `새 일기`
+  }, [])
+
   return (
     <div>
       <DiaryEditor></DiaryEditor>
